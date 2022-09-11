137c9ba04ff92afb657d2742e4f6b5509fd286ef
Yumetsuki11 <shar2107@yandex.ru>
2022-09-11 21:56:52 +0300

    Failing test added (expecting 'amogus')

---
+++ src/test/java/ru/netology/PostTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+    public void shouldPostToPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=Shift-JIS")
+                .body("フリーダが大好き")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("amogus"))
+        ;
+    }
+}
